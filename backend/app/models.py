fb9d9901d277e08ebcd42cf33f05d390dfb6bd32
batuhanerdemirsoftware <88884739+batuhanerdemirsoftware@users.noreply.github.com>
2025-03-01 14:57:46 +0300

    Product model
    Scrum-21

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-class product(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField()
-
-    def __str__(self):
-        return self.name
-
 class Customer(models.Model):
     # Django automatically creates an AutoField named 'id' as primary key.
     name = models.CharField(max_length=100)
@@ -17,3 +9,17 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name        
+
+class Product(models.Model):
+    # Django will create an 'id' primary key automatically unless you override it.
+    name = models.CharField(max_length=100)
+    model = models.CharField(max_length=100, blank=True, null=True)
+    serial_number = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    quantity_in_stock = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    warranty_status = models.CharField(max_length=50, blank=True, null=True) #Under warranty True or Not 
+    distributor_information = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.model}"
