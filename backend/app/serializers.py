1b5e8e9add03fab49297da57ad058f1114cff4ef
Kerem <151570073+keremacildi@users.noreply.github.com>
2025-03-01 16:45:39 +0300

    Update serializers.py

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -1,15 +1,1 @@
-from rest_framework import serializers
-from .models import Product, Review
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ['id', 'name', 'description', 'sale_price', 'cost_price']
-
-class ReviewSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
-    created_at = serializers.ReadOnlyField()
-
-    class Meta:
-        model = Review
-        fields = ['id', 'product', 'user', 'rating', 'comment', 'created_at']
