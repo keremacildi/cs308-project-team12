4fbbee4976540479c264805a0d4b26437f73d3e2
Çağlar Uysal <149569662+chago46@users.noreply.github.com>
2025-03-06 17:25:20 +0300

    Update serializers.py
    beyle daha eyi

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import Product, Review
 
 class ProductSerializer(serializers.ModelSerializer):
+    sale_price = serializers.DecimalField(source='price', max_digits=10, decimal_places=2)
+    cost_price = serializers.DecimalField(source='cost', max_digits=10, decimal_places=2)
+
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'sale_price', 'cost_price']
